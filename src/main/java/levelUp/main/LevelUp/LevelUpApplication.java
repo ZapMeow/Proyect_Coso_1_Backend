beccc8822c18be0199489433a814125149de7616
CattoElectrico <lucas8422olmedoj@gmail.com>
2025-11-13 19:39:52 -0300

    fix again

--- src/main/java/levelUp/main/LevelUp/LevelUpApplication.java
+++ src/main/java/levelUp/main/LevelUp/LevelUpApplication.java
@@ -1,5 +1,8 @@
 package levelUp.main.LevelUp;
 
+import levelUp.main.LevelUp.model.Product;
+import levelUp.main.LevelUp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LevelUpApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LevelUpApplication.class, args);
+
 	}
 
 }
